97ea1cbe2ae70e1f31d47a95617473fe511adb5e
tomekrogalski <tomasz.rogalski@euco.pl>
2017-01-15 11:54:06 +0100

    Zmieniony kod HTML na stronie.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7jjt2pkkrp7pwtx4j64r9+ua8a&@s+@$hl$6)niwsi7($(27#d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
